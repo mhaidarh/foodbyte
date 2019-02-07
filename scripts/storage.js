180ee349e5502994f0f690adf4d6b13733dcf171
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-07 21:34:30 +0700

    Implement Storage service

---
+++ scripts/storage.js
@@ -0,0 +1,11 @@
+const Storage = {
+  // ---------------------------------------------------------------------------
+  set: (key, value) => {
+    window.localStorage.setItem(key, JSON.stringify(value))
+  },
+
+  // ---------------------------------------------------------------------------
+  get: key => {
+    return JSON.parse(window.localStorage.getItem(key))
+  }
+}
